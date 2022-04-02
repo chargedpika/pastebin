5d10767f91a0654f34fc17f1614b0f51992a4e9b
Jonah Hornak <45068818+chargedpika@users.noreply.github.com>
2022-04-02 15:14:59 -0400

    Update gettingDistance.java

--- gettingDistance.java
+++ gettingDistance.java
@@ -15,13 +15,13 @@ double targetOffsetAngle_Vertical = ty.getDouble(0.0);
 double limelightHeightInches = 27.5;
 
 // how many degrees back is your limelight rotated from perfectly vertical?
-double limelightMountAngleDegrees = 30.0;
+double limelightMountAngleDegrees = 25.0;
 
 // distance from the center of the Limelight lens to the floor
 double limelightLensHeightInches = 27.9375;
 
 // distance from the target to the floor
-double goalHeightInches = 103.0;
+double goalHeightInches = 104.0;
 
 double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
 double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
